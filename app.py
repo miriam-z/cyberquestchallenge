0532514a01c1ad0861afad9b058f885f7405c788
miriam-z <miriam_z@icloud.com>
2024-04-29 17:56:11 +0800

    BIG integration with resume logic

--- app.py
+++ app.py
@@ -13,11 +13,23 @@ import chainlit as cl
 from chainlit.types import AskFileResponse
 
 
+# RESUME LOGIC
+from operator import itemgetter
+
+from langchain_community.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
+from langchain.schema.runnable.config import RunnableConfig
+from chainlit.types import ThreadDict
+# RESUME LOGIC
+
+
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, chunk_overlap=100)
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-welcome_message = """Welcome to the Chainlit PDF QA demo! To get started:
+welcome_message = """Welcome to the CyberQuest Challenge: A PDF QA demo! To get started:
 1. Upload a PDF file
 2. Ask a question about the file
 """
@@ -57,8 +69,14 @@ def get_docsearch(file: AskFileResponse):
 
 @cl.on_chat_start
 async def start():
+    
+    # Resume Logic
+    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+    setup_runnable()
+    # Resume Logic
+    
     await cl.Avatar(
-        name="Chatbot",
+        name="CyberQuest Challenge",
         url="https://avatars.githubusercontent.com/u/128686189?s=400&u=a1d1553023f8ea0921fba0debbe92a8c5f840dd9&v=4",
     ).send()
     files = None
@@ -86,8 +104,6 @@ async def start():
         chat_memory=message_history,
         return_messages=True,
     )
-
- 
 
     chain = ConversationalRetrievalChain.from_llm(
         ChatAnthropic(
@@ -121,10 +137,28 @@ async def start():
     cl.user_session.set("message_history", message_history)
 
 
-
-
 @cl.on_message
 async def main(message: cl.Message):
+    
+    # RESUME LOGIC
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
+
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    res = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await res.stream_token(chunk)
+
+    await res.send()
+
+    memory.chat_memory.add_user_message(message.content)
+    memory.chat_memory.add_ai_message(res.content)
+    # RESUME LOGIC
+    
     chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
     cb = cl.AsyncLangchainCallbackHandler()
     res = await chain.acall(message.content, callbacks=[cb])
@@ -148,3 +182,46 @@ async def main(message: cl.Message):
             answer += "\nNo sources found"
 
     await cl.Message(content=answer, elements=text_elements).send()
+    
+    
+# Add Resume-Chat Functionality    
+def setup_runnable():
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful chatbot"),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{question}"),
+        ]
+    )
+
+    runnable = (
+        RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        )
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+    cl.user_session.set("runnable", runnable)
+
+    
+@cl.password_auth_callback
+def auth():
+    return cl.User(identifier="test")
+
+
+@cl.on_chat_resume
+async def on_chat_resume(thread: ThreadDict):
+    memory = ConversationBufferMemory(return_messages=True)
+    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
+    for message in root_messages:
+        if message["type"] == "user_message":
+            memory.chat_memory.add_user_message(message["output"])
+        else:
+            memory.chat_memory.add_ai_message(message["output"])
+
+    cl.user_session.set("memory", memory)
+
+    setup_runnable()    
